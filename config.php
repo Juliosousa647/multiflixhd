aa2c4ecf5dc98eca64ec8b4f79b506fe850dc288
Juliosousa647 <39865197+Juliosousa647@users.noreply.github.com>
2023-07-03 02:12:05 -0300

    commit inicial

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+$CONFIG['SiteTitle']    =   'Séries Online Grátis HD';
+$CONFIG['BaseURI']  =   '';
+$CONFIG['BaseAdmURI']   =   'admin';
+
+
+$CONFIG['Hostname'] =   'mysql.hostinger.com';
+$CONFIG['Username'] =   'u376327204_serie';
+$CONFIG['Password'] =   'Julio647';
+$CONFIG['Database'] =   'u376327204_serie';
+
+
+$CONFIG['AdminUser']    =   'admin';
+$CONFIG['AdminPass']    =   'julio647';
+
+/*
+ To use encode: 
+ Upload Gkencode.php to /theme/
+ Upload gkplugins_encode.swf,license.php And license.swf To /theme/gkplugins/
+*/
+$CONFIG['UseGkEncode']  =   false;
+$CONFIG['GkSecretKey']  =   '';
+$CONFIG['GkPrefix'] =   'myprefix'; //WithOut *  (Not Use Ex: 'myprefix*' Use 'myprefix')
+
+$CONFIG['ImgurApiKey']  =   'c5f42c106a19a485cac10cf3d518d0d9';
+?>
